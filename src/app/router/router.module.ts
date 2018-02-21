44628ac72072d179b5577b191c00ce423a20f0d1
ilcanna <illnicolacannato@gmail.com>
2018-02-21 11:47:28 +0100

    configurazione routing

--- src/app/router/router.module.ts
+++ src/app/router/router.module.ts
@@ -1,13 +1,19 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { DetailComponent } from '../detail/detail.component';
+import { listaComponent } from '../listaComponent/list.component';
 
 const heroesRoutes: Routes = [
-    
+    {path: "home", component: HomeComponent},
+    {path: "list", component: listaComponent},
+    {path: "detail/:id", component: DetailComponent},
+    {path: "", redirectTo: "home", pathMatch: "full"}
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forChild(heroesRoutes)
+    RouterModule.forRoot(heroesRoutes)
   ],
   exports: [
     RouterModule
